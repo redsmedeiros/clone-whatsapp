6d95d9087ee900d52d7b52206f3a21cb38cc07a6
Rodolpho <redsmedeiros@gmail.com>
2022-11-20 16:40:19 -0300

    formdata

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,8 +3,82 @@ class WhatsAppController {
     constructor(){
 
         this.elementsPrototype()
-        
         this.loadElements()
+        this.initEvents()
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e => {
+
+            this.closeAllleftPanel()
+
+            this.el.panelEditProfile.show()
+
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open')
+            }, 300)
+
+
+        })
+
+        this.el.btnNewContact.on('click', e => {
+
+            this.closeAllleftPanel()
+
+            this.el.panelAddContact.show()
+
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open')
+            }, 300)
+            
+        })
+
+        this.el.btnClosePanelEditProfile.on('click', e =>{
+
+            this.el.panelEditProfile.removeClass('open')
+
+        })
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+
+            this.el.panelAddContact.removeClass('open')
+        })
+
+        this.el.photoContainerEditProfile.on('click', e =>{
+
+            this.el.inputProfilePhoto.click()
+        })
+
+        this.el.inputNamePanelEditProfile.on('keypress', e =>{
+
+            if(e.key === 'Enter'){
+
+                e.preventDefault()
+                this.el.btnSavePanelEditProfile.click()
+            }
+
+        })
+
+        this.el.btnSavePanelEditProfile.on('click', e =>{
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML)
+        })
+
+        this.el.formPanelAddContact.on('submit', e => {
+
+            e.preventDefault()
+
+            let formData = new FormData(this.el.formPanelAddContact)
+        })
+
+    }
+
+    closeAllleftPanel(){
+
+        this.el.panelAddContact.hide()
+
+        this.el.panelEditProfile.hide()
     }
 
     loadElements(){
@@ -26,14 +100,20 @@ class WhatsAppController {
         Element.prototype.hide = function(){
             //acessa do objeto html o style.display
             this.style.display = 'none'
+             //para aninhar metodos
+             return this
         }
 
         Element.prototype.show = function(){
             this.style.display = 'block'
+            //para aninhar metodos
+            return this
         }
 
         Element.prototype.toggle = function(){
             this.style.display = (this.style.display === 'none') ? 'block' : 'none'
+             //para aninhar metodos
+             return this
         }
 
         Element.prototype.on = function(eventos, callback){
@@ -42,6 +122,8 @@ class WhatsAppController {
 
                 this.addEventListener(evento, callback)
             })
+             //para aninhar metodos
+             return this
 
         }
 
@@ -51,10 +133,48 @@ class WhatsAppController {
 
                 this.style[nome] = estilos[nome]
             }
+             //para aninhar metodos
+             return this
         }
 
         Element.prototype.addClass = function(nome){
+            this.classList.add(nome)
+             //para aninhar metodos
+             return this
+        }
 
+        Element.prototype.removeClass = function(nome){
+            this.classList.remove(nome)
+             //para aninhar metodos
+             return this
+        }
+
+        Element.prototype.toggleClass = function(nome){
+            this.classList.toggle(nome)
+             //para aninhar metodos
+             return this
+        }
+
+        Element.prototype.hasClass = function(nome){
+            return this.classList.contains(nome)
+             //para aninhar metodos
+             
+        }
+
+        HTMLFormElement.prototype.getForm = function(){
+
+            return new FormData(this)
+        }
+
+        HTMLFormElement.prototype.toJSON = function(){
+
+            let json = {}
+
+            this.getForm().forEach( (elemento, key)=> {
+
+                json[key] = elemento
+                
+            })
         }
 
     }
